28515d1d8ac8f078ec177ed0d5afe6cfb4cd8bc1
Dimas Sulz <dimassulz@gmail.com>
2023-04-06 21:14:26 -0300

    chore: adjusts

--- 7-poo-heranca/js/main.js
+++ 7-poo-heranca/js/main.js
@@ -17,15 +17,26 @@ const monitor = new ProdutoEletronico("AOC 23 polegadas", 1000, 1.2);
 console.log(monitor instanceof ProdutoEletronico);
 console.log(monitor.mostrarDescricao());
 
-//PRODUTOS ELETRONICOS CELULAR -> PRODUTOS ELETRONICOS 
+//PRODUTOS ELETRONICOS CELULAR -> PRODUTOS ELETRONICOS
 
-const celular = new ProdutoEletronicoCelular('Iphone 14 plus', 6000, 0.2, 'Apple')
+const celular = new ProdutoEletronicoCelular(
+  "Iphone 14 plus",
+  6000,
+  0.2,
+  "Apple"
+);
 console.log(celular instanceof Produto);
 console.log(celular.mostrarDescricao());
 console.log(celular.calcularImpostoVenda());
 //console.log(celular.calcularMargemDeRevenda()); //error
 
 //PRODUTO ALIMENTO -> PRODUTO
-const pizza = new ProdutoAlimento('Pizza Muçarela', 40, true, false, '2023-4-7')
+const pizza = new ProdutoAlimento(
+  "Pizza Muçarela",
+  40,
+  true,
+  false,
+  "2023-4-7"
+);
 console.log(pizza instanceof Produto);
 console.log(pizza.mostrarDescricao());
